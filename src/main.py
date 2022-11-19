afe9daae66734dfedcc619807c35c2b9df8245ae
Gako358 <gako358@outlook.com>
2022-11-19 16:19:11 +0100

    working, tested on local and remote repos

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
-import os, time, sys
+import os
 import multiprocessing as mp
 import configparser
 
 from git import Repo
-from colorama import just_fix_windows_console
-from termcolor import colored
 
 
 class Scram(object):
@@ -26,7 +24,9 @@ class Scram(object):
 
         if os.path.isdir(path):
             self.repo = Repo(path)
-            print(f'Fetching updates for : {os.path.basename(path[:-1]):19} Diff : {self.repo.git.pull("origin", "main")}')
+            print(
+                f'Fetching updates for : {os.path.basename(path[:-1]):19} Diff : {self.repo.git.pull("origin", "main")}\n'
+            )
         else:
             os.makedirs(path)
             self.repo = Repo.clone_from(repo, path, env={"GIT_SSH": user["ssh"]})
@@ -38,22 +38,26 @@ class Scram(object):
     def commit(self, branch: str = "main", message: str = "Auto commit"):
         has_changes = False
         for file in self.repo.untracked_files:
-            print(f'Adding untracked file : {file} {os.path.basename(self.path[:-1])}\n')
+            print(
+                f"Adding untracked file : {file} {os.path.basename(self.path[:-1])}\n"
+            )
             self.repo.git.add(file)
             has_changes = True
 
         if self.repo.is_dirty():
-            for file in self.repo.git.diff(None, name_only=True).splitLines():
+            for file in self.repo.git.diff(None, name_only=True).split("\n"):
                 if file == "":
                     continue
 
-                print(f'Adding modified file : {file} {os.path.basename(self.path[:-1])}\n')
+                print(
+                    f"Adding modified file : {file} {os.path.basename(self.path[:-1])}\n"
+                )
                 self.repo.git.add(file)
                 has_changes = True
 
         return has_changes
 
-    def remote(output_queue, input_queue, remote_repo, path_repo):
+    def remote_repos(output_queue, input_queue, remote_repo, path_repo):
         user = configparser.ConfigParser()
         __location__ = os.path.realpath(
             os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -62,64 +66,67 @@ class Scram(object):
         repository = Scram(
             repo=remote_repo, user=dict(user.items("user_id")), path=path_repo
         )
-
         if repository.commit() is True:
-            output_queue.put(colored(f'\nCommiting changes to', 'red'), colored(f'{os.path.basename(path_repo[:-1])} = ', 'green'))
+            output_queue.put(
+                f"\nINPUT: Write commit msg for , {os.path.basename(path_repo[:-1])} = "
+            )
             result = input_queue.get()
-            repository.repo.git.commit('-m', result)
-            repository.repo.git.push('origin', 'main')
-        output_queue.put(colored('Done', 'green'))
+            repository.repo.git.commit("-m", result)
+            repository.repo.git.push("origin", "main")
+        output_queue.put("DONE")
 
-    def main():
-        process = configparser.ConfigParser()
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        process.read(os.path.join(__location__, 'repos.ini'))
-        queues = []
-        num_processes = 0
-
-        for i, name in enumerate(process):
-            if name == 'DEFAULT':
-                continue
-            iq = mp.Queue()
-            oq = mp.Queue()
-            queues.append((oq, iq))
-            mp.Process(target=Scram.remote, args=(oq, iq, process.get(name, 'repo'), process.get(name, 'path'))).start()
-            num_processes += 1
-
-        done = 0
-        waiting_input = 0
-        keep = []
-
-        while done + waiting_input < num_processes:
-            for iq, oq in queues:
-                if not oq.empty():
-                    req = oq.get()
-                    if 'INPUT' in req:
-                        waiting_input += 1
-                        keep.append((req, iq))
-                    elif 'DONE' in req:
-                        done += 1
-                    else:
-                        print(req)
-
-        done = 0
-
-        # keep inputs until all processes are done
-        for req, iq in keep:
-            res = input(req.split(":")[1])
-            iq.put(res)
-
-        while done < waiting_input:
-            for iq, oq in queues:
-                if not oq.empty():
-                    req = oq.get()
-                    if 'DONE' in req:
-                        done += 1
-                    else:
-                        print(req)
-
-        print('\nCompleted all updates')
 
 if __name__ == "__main__":
-    Scram.main()
+    process = configparser.ConfigParser()
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__))
+    )
+    process.read(os.path.join(__location__, "repos.ini"))
+    queues = []
+    num_processes = 0
 
+    for i, name in enumerate(process):
+        if name == "DEFAULT":
+            continue
+        iq = mp.Queue()
+        oq = mp.Queue()
+        queues.append((iq, oq))
+        mp.Process(
+            target=Scram.remote_repos,
+            args=(oq, iq, process.get(name, "repo"), process.get(name, "path")),
+        ).start()
+        num_processes += 1
+
+    done = 0
+    waiting_input = 0
+    keep = []
+
+    while done + waiting_input < num_processes:
+        for iq, oq in queues:
+            if not oq.empty():
+                req = oq.get()
+                if "INPUT" in req:
+                    waiting_input += 1
+                    keep.append((req, iq))
+                elif "DONE" in req:
+                    done += 1
+                else:
+                    print(req)
+
+    done = 0
+
+    # keep inputs until all processes are done
+    for req, iq in keep:
+        res = input(req.split(":")[1])
+        iq.put(res)
+
+    while done < waiting_input:
+        for iq, oq in queues:
+            if not oq.empty():
+                req = oq.get()
+                if "DONE" in req:
+                    done += 1
+                else:
+                    print(req)
+
+    print("\nCompleted all updates")
