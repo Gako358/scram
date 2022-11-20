bd34e83a2cb01fa0552a59f8f5e5b2cd9ad1f95b
Gako358 <gako358@outlook.com>
2022-11-20 03:05:05 +0100

    flake give correct output, but no bin

--- flake.nix
+++ flake.nix
@@ -1,41 +1,53 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
     { self
     , nixpkgs
+    , flake-utils
     ,
     }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      scramgit = pkgs.python39Packages.buildPythonApplication {
+        pname = "scramgit";
+        version = "0.1.0";
+        src = ./src;
+        propagatedBuildInputs = with pkgs.python39Packages; [
+          colorama
+          GitPython
+          termcolor
+        ];
+
+        meta = {
+          description = "A simple git wrapper to help you keep your git history clean";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ merrinx ];
+        };
+      };
     in
     {
-      devShells = forAllSystems (system: {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            python39Packages.colorama
-            python39Packages.GitPython
-            python39Packages.termcolor
-          ];
-        };
-      });
-      packages.${system}.scramgit = forAllSystems (system: {
-        default = pkgs.${system}.buildPythonPackage rec {
-          pname = "scramgit";
-          version = "0.1.0";
-          src = ./src;
-          propagatedBuildInputs = with pkgs.${system}.python39Packages; [
-            colorama
-            GitPython
-            termcolor
-          ];
-        };
-      });
-      apps.default = {
+      packages.scramgit = scramgit;
+      packages.default = self.packages.${system}.scramgit;
+
+      apps.scramgit = {
         type = "app";
         program = "${self.packages.${system}.scramgit}/bin/scramgit";
       };
-    };
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python39Packages.colorama
+          python39Packages.GitPython
+          python39Packages.termcolor
+        ];
+      };
+    });
 }
